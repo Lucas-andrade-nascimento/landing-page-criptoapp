0a5f24789ecc8d5f783cc36ba981949c5408e2cc
Lucas Andrade <andradedesa.lucas@gmail.com>
2022-07-16 21:32:18 -0300

    Cotacao

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,9 @@ fetch(`https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cte
         return resposta.json() 
 
 }).then(corpo=>{ 
-    document.getElementById("btc").innerHTML += " R$" + corpo.bitcoin.brl
-    document.getElementById("eth").innerHTML += corpo.ethereum.usd  
-    document.getElementById("eth").innerHTML += " R$" + corpo.ethereum.brl
-    document.getElementById("tet").innerHTML += corpo.tether.usd  
-    document.getElementById("tet").innerHTML += " R$" + corpo.tether.brl
+    document.getElementById("btc").innerHTML += `${corpo.bitcoin.usd},00 R$${corpo.bitcoin.brl},00 `
+    document.getElementById("eth").innerHTML += `${corpo.ethereum.usd},00 R$${corpo.ethereum.brl},00`
+    document.getElementById("tet").innerHTML += `${corpo.tether.usd},00 R$${corpo.tether.brl},00`
 })
+
 
