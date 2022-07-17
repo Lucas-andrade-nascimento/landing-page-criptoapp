b93a3b626f0dc170521c631fd7fb5f7ccffc3927
daniel <daniellfilipe.nada@gmail.com>
2022-07-17 12:28:17 -0300

    cotações update

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,12 @@ fetch(`https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cte
         return resposta.json() 
 
 }).then(corpo=>{ 
-    document.getElementById("btc").innerHTML += `${corpo.bitcoin.usd},00 R$${corpo.bitcoin.brl},00 `
-    document.getElementById("eth").innerHTML += `${corpo.ethereum.usd},00 R$${corpo.ethereum.brl},00`
-    document.getElementById("tet").innerHTML += `${corpo.tether.usd},00 R$${corpo.tether.brl},00`
+    document.getElementById("btc-dolar").innerHTML += `$${corpo.bitcoin.usd},00`
+    document.getElementById("eth-dolar").innerHTML += `$${corpo.ethereum.usd},00`
+    document.getElementById("tet-dolar").innerHTML += `$${corpo.tether.usd},00`
+    document.getElementById("btc-real").innerHTML += `R$${corpo.bitcoin.brl},00 `
+    document.getElementById("eth-real").innerHTML += `R$${corpo.ethereum.brl},00`
+    document.getElementById("tet-real").innerHTML += `R$${corpo.tether.brl},00`
 })
 
 
